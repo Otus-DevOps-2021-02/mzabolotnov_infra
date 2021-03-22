299710333178df479910baf1ecc6fbd5628c34df
Mikhail Zabolotnov <m.zabolotnov@gmail.com>
2021-03-22 22:46:44 +0400

    Bash-scripts added for deploy app

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+apt-get -y update
+apt-get -y install git
+cd /home/yc-user
+git clone -b monolith https://github.com/express42/reddit.git
+cd reddit && bundle install
+puma -d
---
+++ install_ruby.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+apt-get -y update
+sudo apt install -y ruby-full ruby-bundler build-essential
