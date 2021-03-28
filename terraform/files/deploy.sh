7d2338b4d2666e6177bde80f0f6f1c47d957a9cc
Mikhail Zabolotnov <m.zabolotnov@gmail.com>
2021-03-28 16:20:48 +0400

    Added variables

--- terraform/files/deploy.sh
+++ terraform/files/deploy.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -e
 APP_DIR=${1:-$HOME}
-sudo apt-get install -y git
+sudo apt update -y
+sleep 45
+sudo apt install -y git
+sleep 45
 git clone -b monolith https://github.com/express42/reddit.git $APP_DIR/reddit
 cd $APP_DIR/reddit
 bundle install
