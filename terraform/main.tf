cf78daf9920f9a74da8a1e6fb6378845417d60aa
Mikhail Zabolotnov <m.zabolotnov@gmail.com>
2021-04-02 09:38:05 +0400

    Using modules
    Signed-off-by: Mikhail Zabolotnov <m.zabolotnov@gmail.com>

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,3 +5,18 @@ provider "yandex" {
  folder_id = var.folder_id
  zone = var.zone
 }
+
+module "app" {
+  source          = "./modules/app"
+  public_key_path = var.public_key_path
+  app_disk_image  = var.app_disk_image
+  # subnet_id       = var.subnet_id
+  subnet_id       = yandex_vpc_subnet.app-subnet.id
+}
+
+module "db" {
+  source          = "./modules/db"
+  public_key_path = var.public_key_path
+  db_disk_image   = var.db_disk_image
+  subnet_id       = yandex_vpc_subnet.app-subnet.id
+}
