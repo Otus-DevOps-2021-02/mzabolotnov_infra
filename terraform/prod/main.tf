a6f575e8a68a245496fdfa8a32a3f16248093594
Mikhail Zabolotnov <m.zabolotnov@gmail.com>
2021-04-16 13:34:54 +0400

    HW start

--- terraform/prod/main.tf
+++ terraform/prod/main.tf
@@ -1,34 +1,40 @@
 
 provider "yandex" {
- service_account_key_file = var.service_account_key_file
- cloud_id = var.cloud_id
- folder_id = var.folder_id
- zone = var.zone
+  service_account_key_file = var.service_account_key_file
+  cloud_id                 = var.cloud_id
+  folder_id                = var.folder_id
+  zone                     = var.zone
 }
-resource "yandex_vpc_network" "app-network" {
-  name = "reddit-app-network"
-}
+# provider "yandex" {
+#  service_account_key_file = var.service_account_key_file
+#  cloud_id = var.cloud_id
+#  folder_id = var.folder_id
+#  zone = var.zone
+# }
+# resource "yandex_vpc_network" "app-network" {
+#   name = "reddit-app-network"
+# }
 
-resource "yandex_vpc_subnet" "app-subnet" {
-  name           = "reddit-app-subnet"
-  zone           = "ru-central1-b"
-  network_id     = "${yandex_vpc_network.app-network.id}"
-  v4_cidr_blocks = ["192.168.10.0/24"]
-}
+# resource "yandex_vpc_subnet" "app-subnet" {
+#   name           = "reddit-app-subnet"
+#   zone           = "ru-central1-b"
+#   network_id     = "${yandex_vpc_network.app-network.id}"
+#   v4_cidr_blocks = ["192.168.10.0/24"]
+# }
 
 module "app" {
   source          = "../modules/app"
   public_key_path = var.public_key_path
   app_disk_image  = var.app_disk_image
   privat_key_path = var.privat_key_path
-  #subnet_id       = var.subnet_id
-  subnet_id       = yandex_vpc_subnet.app-subnet.id
+  subnet_id       = var.subnet_id
+  # subnet_id       = yandex_vpc_subnet.app-subnet.id
 }
 
 module "db" {
   source          = "../modules/db"
   public_key_path = var.public_key_path
   db_disk_image   = var.db_disk_image
-  # subnet_id       = var.subnet_id
-  subnet_id       = yandex_vpc_subnet.app-subnet.id
+  subnet_id       = var.subnet_id
+  # subnet_id       = yandex_vpc_subnet.app-subnet.id
 }
